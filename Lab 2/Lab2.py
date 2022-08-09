2dd3b04940a196bc9b5315a58e53eaa65c4d5e4f
Tonkhaow <Tonkhaowm8@gmail.com>
2022-08-09 11:44:50 +0700

    ad to decimal function

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -11,7 +11,22 @@ def from_decimal(d, b):
     remLs.reverse()
     return remLs
 
+def to_decimal(d, b):
+    strList = str(d).split()
+    strList.reverse()
+    dec = 0
+    for i in range(len(strList)):
+        dec += int(strList[i]) * b**i
+    return dec
 
-deciInput = int(input("Enter the decimal: "))
-decibase = int(input("Enter the base: "))
-print(from_decimal(deciInput, decibase))
+selectConvert = int(input("1 to convert from decimal, 2 to convert to decimal: "))
+if selectConvert == 1:
+    deciInput = int(input("Enter the decimal: "))
+    decibase = int(input("Enter the base: "))
+    print(from_decimal(deciInput, decibase))
+elif selectConvert == 2:
+    toDecInput = int(input("Enter the decimal: "))
+    toDecBase = int(input("Enter the base: "))
+    print(from_decimal(toDecInput, toDecBase))
+else:
+    print("error")
