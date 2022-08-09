0d7918d4303932f59f54e7f993fa7698e4846ac1
Tonkhaow <Tonkhaowm8@gmail.com>
2022-08-09 11:15:38 +0700

    add function to_decimal

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -2,5 +2,16 @@
 
 def from_decimal(d, b):
     i = d
+    remLs = []
     while i != 0:
-        
+        rem = i % b
+        print(rem)
+        remLs.append(rem)
+        i = i//b
+    remLs.reverse()
+    return remLs
+
+
+deciInput = int(input("Enter the decimal: "))
+decibase = int(input("Enter the base: "))
+print(from_decimal(deciInput, decibase))
