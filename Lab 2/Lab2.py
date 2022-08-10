2fcdd65e239e7422ed79b6251f2d2867abc8af56
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-11 00:06:26 +0700

    add hexadecimal feature

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -1,5 +1,6 @@
  # Siraphop Mukdaphetcharat 64011614
 hexDict = {"A": 10, "B" : 11, "C" : 12, "D" : 13, "E" : 14, "F" : 15} #hex dictionary
+hexLs = ["A", "B", "C", "D", "E", "F"]
 
 def from_decimal(d, b):
     i = d
@@ -13,11 +14,18 @@ def from_decimal(d, b):
     return remLs
 
 def to_decimal(d, b):
+    numLs = []
     strList = [*str(d)]
     strList.reverse()
+    for i in strList:
+        try:
+            i.capitalize()
+            numLs.append(int(hexDict[i]))
+        except:
+            numLs.append(int(i))
     dec = 0
-    for i in range(len(strList)):
-        dec += int(strList[i]) * (b**i)
+    for i in range(len(numLs)):
+        dec += int(numLs[i]) * (int(b)**i)
     return dec
 
 selectConvert = int(input("1 to convert from decimal, 2 to convert to decimal: "))
@@ -26,8 +34,8 @@ if selectConvert == 1:
     decibase = int(input("Enter the base: "))
     print(from_decimal(deciInput, decibase))
 elif selectConvert == 2:
-    toDecInput = int(input("Enter the decimal: "))
-    toDecBase = int(input("Enter the base: "))
+    toDecInput = input("Enter the power: ")
+    toDecBase = input("Enter the base: ")
     print(to_decimal(toDecInput, toDecBase))
 else:
     print("error")
