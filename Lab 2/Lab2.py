1b52f0549b15aaed1437477fe0db322ee251aa8a
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-12 01:40:33 +0700

    can convert to base 35 without hex dict

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -1,6 +1,6 @@
  # Siraphop Mukdaphetcharat 64011614
-hexDict = {"A": 10, "B" : 11, "C" : 12, "D" : 13, "E" : 14, "F" : 15, "G" : 16, "H" : 17, "I" : 18, "J" : 19, "K" : 20,"L" : 21,"M" : 22,"N" : 23,
-"O" : 24,"P" : 25,"Q" : 26,"R" : 27,"S" : 28,"T" : 29,"U" : 30,"V" : 31,"W" : 32,"X" : 33,"Y" : 34,"Z" : 35} #hex dictionary
+#hexDict = {"A": 10, "B" : 11, "C" : 12, "D" : 13, "E" : 14, "F" : 15, "G" : 16, "H" : 17, "I" : 18, "J" : 19, "K" : 20,"L" : 21,"M" : 22,"N" : 23,
+#"O" : 24,"P" : 25,"Q" : 26,"R" : 27,"S" : 28,"T" : 29,"U" : 30,"V" : 31,"W" : 32,"X" : 33,"Y" : 34,"Z" : 35} #hex dictionary
 
 def from_decimal(d, b):
     i = d
@@ -20,7 +20,11 @@ def to_decimal(d, b):
     for i in strList:
         try:
             i.capitalize()
-            numLs.append(int(hexDict[i]))
+            translated = ord(i) - 55
+            if translated <= int(b):
+                numLs.append(translated)
+            else:
+                print("error! Invalid Input")
         except:
             numLs.append(int(i))
     dec = 0
