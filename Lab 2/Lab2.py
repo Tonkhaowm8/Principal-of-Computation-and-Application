1d4f113434d9268afe1ccd1228c4ee91af00e7b9
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-12 01:16:36 +0700

    completed hex dict

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -1,5 +1,6 @@
  # Siraphop Mukdaphetcharat 64011614
-hexDict = {"A": 10, "B" : 11, "C" : 12, "D" : 13, "E" : 14, "F" : 15} #hex dictionary
+hexDict = {"A": 10, "B" : 11, "C" : 12, "D" : 13, "E" : 14, "F" : 15, "G" : 16, "H" : 17, "I" : 18, "J" : 19, "K" : 20,"L" : 21,"M" : 22,"N" : 23,
+"O" : 24,"P" : 25,"Q" : 26,"R" : 27,"S" : 28,"T" : 29,"U" : 30,"V" : 31,"W" : 32,"X" : 33,"Y" : 34,"Z" : 35} #hex dictionary
 
 def from_decimal(d, b):
     i = d
@@ -27,13 +28,13 @@ def to_decimal(d, b):
         dec += int(numLs[i]) * (int(b)**i)
     return dec
 
-def start():
+def start(): # Start Function for user input
     try:
-        rep = True
+        rep = True # repeating input for errors
         selectConvert = int(input("1 to convert from decimal, 2 to convert to decimal: "))
         while rep:
             if selectConvert == 1:
-                try:
+                try: #Input from decimal to any base
                     deciInput = int(input("Enter the decimal: "))
                     decibase = int(input("Enter the base: "))
                     print(from_decimal(deciInput, decibase))
