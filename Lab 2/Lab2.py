1ecaea84b08f8aaebf6963e2a38e1986a3625388
Tonkhaow <Tonkhaowm8@gmail.com>
2022-08-16 09:10:32 +0700

    update mac

--- Lab 2/Lab2.py
+++ Lab 2/Lab2.py
@@ -1,32 +1,89 @@
- # Siraphop Mukdaphetcharat 64011614
-
-def from_decimal(d, b):
+# Siraphop Mukdaphetcharat 64011614
+def from_decimal(d, b):  # Convert decimal to any base function
+    ansLs = []
+    if b == 1:
+        print("ERROR! Base cannot be 1!!")
+        exit()
+    elif b > 36:
+        print("ERROR! Base cannot exceed 36!")
+        exit()
+    elif b <= 0 or d <= 0:
+        print("ERROR! Base cannot be less than or equal to zero!")
+        exit()
     i = d
     remLs = []
-    while i != 0:
+    finalLs = []
+    while i != 0:  # Change decimal to base given
         rem = i % b
-        print(rem)
         remLs.append(rem)
-        i = i//b
+        i = i // b
     remLs.reverse()
-    return remLs
+    for i in remLs:  # Convert the number to hex alphabet if the number exceed 9
+        if i <= 9:
+            finalLs.append(i)
+        else:
+            finalLs.append(chr(i + 55))  # chr is ascii function that converts number to characters
+    for i in finalLs:
+        ansLs.append(str(i))
+        ans = "".join(ansLs)
+    return ans
 
-def to_decimal(d, b):
-    strList = [*str(d)]
+
+def to_decimal(d, b):  # Convert any base to decimal
+    if b.isalpha():
+        print("ERROR! Base cannot be an alphabet")
+        exit()
+    elif b == "1" or b == "0":
+        print("ERROR! Base cannot be less than 1!!")
+        exit()
+    elif "-" in b or "-" in d:
+        print("ERROR! Base cannot be less than 1!!")
+        exit()
+    numLs = []
+    strList = [*str(d)]  # convert string without space into arrays
     strList.reverse()
+    for i in strList:
+        try:  # Capitalize everything so that the text would have similar ASCII code
+            i = int(i)
+            numLs.append(int(i))
+        except ValueError:
+            i.capitalize()
+            translated = ord(i) - 55  # ord change ASCII code of text into number
+            if translated <= int(b):
+                numLs.append(translated)
+            else:
+                print("Error! Invalid Input")
+                exit()
     dec = 0
-    for i in range(len(strList)):
-        dec += int(strList[i]) * (b**i)
+    for i in range(len(numLs)):  # calculate base number into decimals
+        dec += int(numLs[i]) * (int(b) ** i)
     return dec
 
-selectConvert = int(input("1 to convert from decimal, 2 to convert to decimal: "))
-if selectConvert == 1:
-    deciInput = int(input("Enter the decimal: "))
-    decibase = int(input("Enter the base: "))
-    print(from_decimal(deciInput, decibase))
-elif selectConvert == 2:
-    toDecInput = int(input("Enter the decimal: "))
-    toDecBase = int(input("Enter the base: "))
-    print(to_decimal(toDecInput, toDecBase))
-else:
-    print("error")
+
+def start():  # Start Function for user input
+    try:
+        rep = True  # repeating input for errors
+        selectConvert = int(input("1 to convert from decimal, 2 to convert to decimal: "))
+        while rep:
+            if selectConvert == 1:
+                try:  # Input from decimal to any base
+                    deciInput = int(input("Enter the decimal: "))
+                    decibase = int(input("Enter the base: "))
+                    print(from_decimal(deciInput, decibase))
+                    rep = False
+                except ValueError:
+                    print("please input a correct input type")
+            elif selectConvert == 2:  # Input from decimal to any base
+                toDecInput = input("Enter the power: ")
+                toDecBase = input("Enter the base: ")
+                print(to_decimal(toDecInput, toDecBase))
+                rep = False
+            else:
+                print("Please select 1 or 2")
+                start()
+    except ValueError:
+        print("Please select 1 or 2")
+        start()
+
+
+start()
