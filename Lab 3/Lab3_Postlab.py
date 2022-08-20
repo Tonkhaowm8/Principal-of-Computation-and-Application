4ab21b67950d2288b0f55f042f5902fdff2c9121
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-20 14:10:27 +0700

    add random function in the in it class

--- Lab 3/Lab3_Postlab.py
+++ Lab 3/Lab3_Postlab.py
@@ -5,9 +5,18 @@ import random
 
 
 class TurtleFRO():
-    def __init__ (self, energy, randomness, color):
+    def __init__ (self, energy, color):
         self.t = turtle.Turtle()
         self.color = turtle.color(color)
+        while energy >= 0:
+            self.rotate = turtle.setheading(random.randint(-360, 360))
+            self.pendown = turtle.pendown()
+            self.stamina = random.randint(-energy, energy)
+            self.move = turtle.forward(self.stamina)
+            energy = energy - abs(self.stamina)
+            print(energy)
         
 
-TurtleFRO(100, 10, "Green")
+TurtleFRO(100, "Green")
+while True:
+    hello = 0
