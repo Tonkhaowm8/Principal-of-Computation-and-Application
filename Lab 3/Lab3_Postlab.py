30a112e9446abe1d234e7b16db0b33d208b86623
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-21 20:28:41 +0700

    make each turtle move

--- Lab 3/Lab3_Postlab.py
+++ Lab 3/Lab3_Postlab.py
@@ -1,3 +1,5 @@
+#Siraphop Mukdaphetcharat 64011614
+
 from mimetypes import init
 from re import T
 import turtle
@@ -6,30 +8,43 @@ import random
 
 class TurtleFRO():
     def __init__ (self, energy, color):
-        self.t = turtle.Turtle(shape = "turtle")
-        self.color = turtle.color(color)
-        self.energy = energy
+        self.t = turtle.Turtle(shape="turtle")
+        self.t.color(color)
 
     def normal(self):
-        self.pendown = turtle.pendown()
-        self.move = turtle.forward(self.energy)
-        self.penup = turtle.penup()
+        self.t.pendown()
+        self.t.forward(self.energy)
+        self.t.penup()
 
     def drunk(self):
-        while self.energy != 0:
-            self.rotate = turtle.setheading(random.randint(-360, 360))
-            self.pendown = turtle.pendown()
-            self.stamina = random.randint(-self.energy, self.energy)
-            self.move = turtle.forward(self.stamina)
-            self.energy = self.energy - abs(self.stamina)
-        self.penup = turtle.penup()
+        self.t.setheading(random.randint(-360, 360))
+        self.t.pendown()
+        self.t.forward(random.randint(-50, 100))
+        self.t.penup()
+        self.t.speed(3)
+
+    def on_crack(self):
+        self.t.setheading(random.randint(-45, 45))
+        self.t.pendown()
+        self.t.forward(random.randint(0, 100))
+        self.t.penup()
+        self.t.speed(10)
+
+    def autistic(self):
+        self.t.setheading(random.randint(-30, 30))
+        self.t.pendown()
+        self.t.forward(random.randint(-50, 10))
+        self.t.penup()
+        self.t.speed(5)
 
 turtle_list = list()
 
-normalTurtle = TurtleFRO(500, "Green")
-normalTurtle.normal()
-drunkTurtle = TurtleFRO(500, "Red")
-drunkTurtle.drunk()
+for i in range(5):
+    turtle_list.append(TurtleFRO(10, "Green"))
 
-while True:
-    hello = 0
+for i in range(100):
+    for j in range(len(turtle_list)):
+        if j % 2 == 0:
+            turtle_list[j].drunk()
+        else:
+            turtle_list[j].on_crack()
