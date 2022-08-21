bdac6b6c571facd079fdac7ff36c8d4d71f40232
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-21 20:28:54 +0700

    add test file

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ colorlist = ["red", "green", "black", "blue", "brown"]
 for i in range(5):
     tlist.append(turtle.Turtle(shape="turtle"))
     tlist[i].color(colorlist[i])
-    tlist[i].speed(10)
+    tlist[i].speed(1)
 screen = turtle.getscreen()
 for i in range(100):
-    screen.tracer(1000)
+    screen.tracer(5)
     for t in tlist:
         t.right((np.random.rand(1) - .5) * 180)
         t.forward(int((np.random.rand(1) - .5) * 100))
