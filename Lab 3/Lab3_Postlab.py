986bcf0d24939c77cb9c0c1dd1d9e8f4afa195b7
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-22 02:15:22 +0700

    add comments

--- Lab 3/Lab3_Postlab.py
+++ Lab 3/Lab3_Postlab.py
@@ -1,27 +1,28 @@
 #Siraphop Mukdaphetcharat 64011614
 
+#imports
 import turtle
 import random
 import numpy as np
 
 class TurtleFRO():
-    def __init__ (self, energy, color):
+    def __init__ (self, energy, color): # Add turtle and colors
         self.t = turtle.Turtle(shape="turtle")
         self.t.color(color)
 
-    def setPos(self, position):
+    def setPos(self, position): #set initial positions for each turtle
         self.t.penup()
         self.t.goto(-400 , position - 250)
 
-    def normal(self, energy):
+    def normal(self, energy): # normal turtle function where it walks as a straight line
         usedEnergy = random.randint(0, 20)
         self.t.pendown()
-        self.t.forward(usedEnergy)
+        self.t.forward(usedEnergy) 
         self.t.penup()
         energy -= usedEnergy
         return energy
 
-    def drunk(self, energy):
+    def drunk(self, energy): # drunk turtle where the turtle will spins randomly
         usedEnergy = random.randint(-50, 100)
         self.t.speed(3)
         self.t.setheading(random.randint(-360, 360))
@@ -31,7 +32,7 @@ class TurtleFRO():
         energy -= usedEnergy
         return energy
 
-    def on_crack(self, energy):
+    def on_crack(self, energy): # where the turtle is super energetic
         usedEnergy = random.randint(0, 100)
         self.t.speed(10)
         self.t.setheading(random.randint(-45, 45))
@@ -41,7 +42,7 @@ class TurtleFRO():
         energy -= usedEnergy
         return energy
         
-    def autistic(self, energy):
+    def autistic(self, energy): # Where the turtle mainly moves backwards
         usedEnergy = random.randint(-10, 20)
         self.t.speed(5)
         self.t.setheading(random.randint(-30, 30))
@@ -51,7 +52,7 @@ class TurtleFRO():
         energy -= usedEnergy
         return energy
 
-    def fat(self, energy):
+    def fat(self, energy): # Where the turtle moves the slowest
         usedEnergy = random.randint(0, 10)
         self.t.speed(10)
         self.t.pendown()
@@ -60,16 +61,16 @@ class TurtleFRO():
         energy -= usedEnergy
         return energy
 
-    def getPos(self):
+    def getPos(self): # Get position of the turtle and return it in x-axis integer value
         t = self.t.pos()
         return t[0]
 
-    def stop(self):
+    def stop(self): # Stop function to stop the turtle from moving
         self.t.forward(0)
 
-turtle_list = list()
+turtle_list = list() # Creating a list of turtles
 
-def line_creator():
+def line_creator(): # Creating a line for starting position and stopping position
     t = turtle.Turtle()
     t.speed(10)
     t.penup()
@@ -85,35 +86,36 @@ def line_creator():
     t.penup()
 
 
-def start():
+def start(): # Start function
 
-    colorArr = ["Red", "Blue", "Green", "Yellow", "Black", "Brown", "Pink", "Orange"]
+    colorArr = ["Red", "Blue", "Green", "Yellow", "Black", "Brown", "Pink", "Orange"] # Color array for selecting colors
    
     try:
-        numTurtle = int(input("Enter number of turtles: "))
-    except ValueError:
+        numTurtle = int(input("Enter number of turtles: ")) # User Input the abount of turtles to spawn
+    except ValueError: # if user enters anything other than integer
         print("Please enter a number!!!")
         start()
-    if numTurtle < 1 or numTurtle > 5:
+    if numTurtle < 1 or numTurtle > 5: # if the number is less then 1 or more than 5
         print("The number of turtle cannot be less than 1 and exceed 5")
         start()
 
     inputArr = []
-    for i in range(numTurtle):
+    for i in range(numTurtle): # asks user to select roles for the turtle they choose
         try:
             userSelection = int(input("(1) Normal Turtle (2) Drunk Turtle (3) On Crack Turtle (4) Autistic Turtle (5) Fat Turtle: "))
             if userSelection < 0 or userSelection > 5:
                 print("Please enter the value between 1 - 5!!!")
+                exit()
             enerArr = [userSelection, random.randint(400, 1000)]
             inputArr.append(enerArr)
-        except ValueError:
+        except ValueError: # if user enters anything other than an integer
             print("Please enter a number!!")
             start()
 
-    turtle.screensize(canvwidth=500, canvheight=500)
+    turtle.screensize(canvwidth=500, canvheight=500) # Set the screen size for the turtle race
     line_creator()
     
-    for i in range(numTurtle):
+    for i in range(numTurtle): # spawning and formatting the turtles
         randNum = random.randint(0,len(colorArr)-1)
         turtle_list.append(TurtleFRO(10, colorArr[randNum]))
         colorArr.pop(randNum)
@@ -123,16 +125,16 @@ def start():
         turtle_list[i].setPos(t[i+1])
 
     finish = False
-    for i in range(100): 
+    for i in range(100):  # Moving the turtle
         for j in range(len(turtle_list)):
-            if turtle_list[j].getPos() >= 400:
+            if turtle_list[j].getPos() >= 400: # When the turtle gets to the finis line
                 finish = True
                 break
             elif inputArr[j][0] == 1:
-                if inputArr[j][1] <=0:
+                if inputArr[j][1] <=0: # No energy left
                     turtle_list[j].stop()
                 else:
-                    inputArr[j][1] = turtle_list[j].normal(inputArr[j][1])
+                    inputArr[j][1] = turtle_list[j].normal(inputArr[j][1]) # Moving Normally
             elif inputArr[j][0] == 2:
                 if inputArr[j][1] <=0:
                     turtle_list[j].stop()
@@ -156,7 +158,7 @@ def start():
             else:
                 continue
                 
-        if finish:
+        if finish: # break when the turtle reaches the finish line
             break
     if finish:
         for k in turtle_list:
