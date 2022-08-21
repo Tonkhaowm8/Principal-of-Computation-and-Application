118f77b3723f1e1ea41eb1d62c6249066c623c64
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-22 02:03:04 +0700

    add energy for each turtle

--- Lab 3/Lab3_Postlab.py
+++ Lab 3/Lab3_Postlab.py
@@ -1,7 +1,5 @@
 #Siraphop Mukdaphetcharat 64011614
 
-from mimetypes import init
-from re import T
 import turtle
 import random
 import numpy as np
@@ -15,37 +13,52 @@ class TurtleFRO():
         self.t.penup()
         self.t.goto(-400 , position - 250)
 
-    def normal(self):
+    def normal(self, energy):
+        usedEnergy = random.randint(0, 20)
         self.t.pendown()
-        self.t.forward(100)
+        self.t.forward(usedEnergy)
         self.t.penup()
+        energy -= usedEnergy
+        return energy
 
-    def drunk(self):
+    def drunk(self, energy):
+        usedEnergy = random.randint(-50, 100)
         self.t.speed(3)
         self.t.setheading(random.randint(-360, 360))
         self.t.pendown()
-        self.t.forward(random.randint(-50, 100))
+        self.t.forward(usedEnergy)
         self.t.penup()
+        energy -= usedEnergy
+        return energy
 
-    def on_crack(self):
+    def on_crack(self, energy):
+        usedEnergy = random.randint(0, 100)
         self.t.speed(10)
         self.t.setheading(random.randint(-45, 45))
         self.t.pendown()
-        self.t.forward(random.randint(0, 100))
+        self.t.forward(usedEnergy)
         self.t.penup()
+        energy -= usedEnergy
+        return energy
         
-    def autistic(self):
+    def autistic(self, energy):
+        usedEnergy = random.randint(-10, 20)
         self.t.speed(5)
         self.t.setheading(random.randint(-30, 30))
         self.t.pendown()
-        self.t.forward(random.randint(-10, 20))
+        self.t.forward(usedEnergy)
         self.t.penup()
+        energy -= usedEnergy
+        return energy
 
-    def fat(self):
-        self.t.speed(5)
+    def fat(self, energy):
+        usedEnergy = random.randint(0, 10)
+        self.t.speed(10)
         self.t.pendown()
-        self.t.forward(random.randint(0, 20))
+        self.t.forward(usedEnergy)
         self.t.penup()
+        energy -= usedEnergy
+        return energy
 
     def getPos(self):
         t = self.t.pos()
@@ -58,6 +71,7 @@ turtle_list = list()
 
 def line_creator():
     t = turtle.Turtle()
+    t.speed(10)
     t.penup()
     t.goto(-400, -500)
     t.setheading(90)
@@ -90,7 +104,8 @@ def start():
             userSelection = int(input("(1) Normal Turtle (2) Drunk Turtle (3) On Crack Turtle (4) Autistic Turtle (5) Fat Turtle: "))
             if userSelection < 0 or userSelection > 5:
                 print("Please enter the value between 1 - 5!!!")
-            inputArr.append(userSelection)
+            enerArr = [userSelection, random.randint(400, 1000)]
+            inputArr.append(enerArr)
         except ValueError:
             print("Please enter a number!!")
             start()
@@ -113,16 +128,31 @@ def start():
             if turtle_list[j].getPos() >= 400:
                 finish = True
                 break
-            elif inputArr[j] == 1:
-                turtle_list[j].normal()
-            elif inputArr[j] == 2:
-                turtle_list[j].drunk()
-            elif inputArr[j] == 3:
-                turtle_list[j].on_crack()
-            elif inputArr[j] == 4:
-                turtle_list[j].autistic()
-            elif inputArr[j] == 5:
-                turtle_list[j].fat()
+            elif inputArr[j][0] == 1:
+                if inputArr[j][1] <=0:
+                    turtle_list[j].stop()
+                else:
+                    inputArr[j][1] = turtle_list[j].normal(inputArr[j][1])
+            elif inputArr[j][0] == 2:
+                if inputArr[j][1] <=0:
+                    turtle_list[j].stop()
+                else:
+                    inputArr[j][1] = turtle_list[j].drunk(inputArr[j][1])
+            elif inputArr[j][0] == 3:
+                if inputArr[j][1] <=0:
+                    turtle_list[j].stop()
+                else:
+                    inputArr[j][1] = turtle_list[j].on_crack(inputArr[j][1])
+            elif inputArr[j][0] == 4:
+                if inputArr[j][1] <=0:
+                    turtle_list[j].stop()
+                else:
+                    inputArr[j][1] = turtle_list[j].autistic(inputArr[j][1])
+            elif inputArr[j][0] == 5:
+                if inputArr[j][1] <=0:
+                    turtle_list[j].stop()
+                else:
+                    inputArr[j][1] = turtle_list[j].fat(inputArr[j][1])
             else:
                 continue
                 
