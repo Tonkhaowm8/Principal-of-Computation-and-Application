361e71ad2dc0adef5a12969167d2576fc7c2036c
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-29 15:29:29 +0700

    add code

--- Lab 4/Lab4-2.py
+++ Lab 4/Lab4-2.py
@@ -1,10 +1,12 @@
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import math
+import random
 
 class Point:
     def __init__(self, x_init, y_init):
         self.x = x_init
         self.y = y_init
+        self.coords = [[],[]]
     
     def get_x(self):
         return self.x
@@ -18,8 +20,26 @@ class Point:
     def __str__(self):
         return "(%s, %s)" % (self.x, self.y)
 
-    #def distance(self, other):
-        #x_distance = 0
-        #dis = math.sqrt(a**2+b**2)
+    def distance(self, a, b, c, d):
+        x_distance = 0
+        dis = math.sqrt((a-c)**2+(b-d)**2)
+        return d
 
-    
+    def findClosest(self):
+        print(len(self.coords[0]))
+
+
+def start():
+    coords = []
+    pointInput = int(input("Enter the number of Points: "))
+    xArr, yArr = [], []
+    for i in range(pointInput):
+        xArr.append(random.randint(0, 1000))
+        yArr.append(random.randint(0, 1000))
+        p = Point(xArr[i], yArr[i])
+        coords.append(p)
+    while 1:
+        plt.plot(xArr, yArr, 'ro')
+        plt.show()
+
+start()
