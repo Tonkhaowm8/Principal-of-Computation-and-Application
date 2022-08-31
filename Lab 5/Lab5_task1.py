f69d9f002e0c28b325dd4191a9f0b24118404a87
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-31 11:55:03 +0700

    turtle

--- Lab 5/Lab5_task1.py
+++ Lab 5/Lab5_task1.py
@@ -1,1 +1,14 @@
+#64011614 Siraphop Mukdaphetcharat
 import turtle
+
+t = turtle.Turtle()
+length = 100
+t.screensize(canvwidth=500, canvheight=500)
+t.penup()
+t.goto(-450, 0)
+def draw(branches, t, length):
+    if branches != 0:
+        t.pendown()
+        t.forward(length)
+        length = length / 2
+        draw(branches-1, t, length)
