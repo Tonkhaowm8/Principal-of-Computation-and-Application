ae3fc8c1c5e15268b8bb8000203186dc41d293d9
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-08-31 12:03:36 +0700

    add condition initial step

--- Lab 5/Lab5_task1.py
+++ Lab 5/Lab5_task1.py
@@ -3,12 +3,21 @@ import turtle
 
 t = turtle.Turtle()
 length = 100
+angle = 20
+first = True
 t.screensize(canvwidth=500, canvheight=500)
 t.penup()
 t.goto(-450, 0)
-def draw(branches, t, length):
+def draw(branches, t, length, first):
     if branches != 0:
-        t.pendown()
-        t.forward(length)
-        length = length / 2
-        draw(branches-1, t, length)
+        if first:
+            t.pendown()
+            t.forward(length)
+            length = length / 2
+        else:
+            t.pendown()
+            t.right(angle)
+            t.forward(length)
+            t.backward(length)
+            t.left(angle * 2)
+        draw(branches-1, t, length, First)
