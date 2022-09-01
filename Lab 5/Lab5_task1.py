9bb75f0174cef52e1d189eacc12db3baebc8ecea
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-09-01 13:19:15 +0700

    add code

--- Lab 5/Lab5_task1.py
+++ Lab 5/Lab5_task1.py
@@ -2,22 +2,23 @@
 import turtle
 
 t = turtle.Turtle()
-length = 100
-angle = 20
-first = True
-t.screensize(canvwidth=500, canvheight=500)
-t.penup()
-t.goto(-450, 0)
-def draw(branches, t, length, first):
+
+def draw(branches, t, length):
     if branches != 0:
-        if first:
-            t.pendown()
-            t.forward(length)
-            length = length / 2
-        else:
-            t.pendown()
-            t.right(angle)
-            t.forward(length)
-            t.backward(length)
-            t.left(angle * 2)
-        draw(branches-1, t, length, First)
+        t.pendown()
+        length = length ** branches
+        t.forward(length)
+        draw(branches-1, t)
+
+def start():
+    length = 5
+    angle = 20
+    first = True
+    t.screensize(canvwidth=500, canvheight=500)
+    t.penup()
+    draw(10, t, length)
+    t.goto(-450, 0)
+
+while True:
+    start()
+    
--- Lab 5/Lab5_task2.py
+++ Lab 5/Lab5_task2.py
@@ -1,2 +1,9 @@
-
+i = 10
 def recursive(i): #hello
+    if i == 0:
+        print(i)
+    else:
+        print(i)
+        recursive(i-1)
+
+recursive(i)
