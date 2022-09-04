b3a4088cbd68fcd0dcfe7bb10139a17294db2361
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-09-04 21:15:02 +0700

    make stars in decending order

--- Lab 5/Lab5_task2.py
+++ Lab 5/Lab5_task2.py
@@ -4,4 +4,12 @@ def stars(n):
         print("*" * stars(n-1))
     return (n + 1)
 
-stars(int(input("enter a number: ")))
+#stars(int(input("enter a number: ")))
+
+def stars2(n):
+    if n > 0:
+        print("*" * stars2(n-1))
+    return (n + 1)
+    
+
+stars2(int(input("enter a number: ")))
