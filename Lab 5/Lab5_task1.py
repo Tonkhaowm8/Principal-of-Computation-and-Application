d91c2fa83def19d53bf6f18069298ad757f5e558
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-09-05 23:15:39 +0700

    modify code so that it fits recursion

--- Lab 5/Lab5_task1.py
+++ Lab 5/Lab5_task1.py
@@ -1,34 +1,19 @@
-#64011614 Siraphop Mukdaphetcharat
+#64011164 Siraphop Mukdaphetcharat
 import turtle
-import numpy as np
+turtle.screensize(500, 500)
+turtle.speed(10)
 
-t = turtle.Turtle()
-
-def draw(branches):
-    if branches >= 0:
-        t.penup()
-        xArr = np.linspace(-400,400, num=branches, endpoint=False)
-        print(xArr)
-        yArr = []
-        for i in range(branches):
-            y = np.linspace(-400, 400, num=(2**(i)), endpoint=False)
-            yArr.append(y)
-        for i in range(1,len(xArr)-1):
-            for j in range(1,len(yArr[i])):
-                t.goto(xArr[i], yArr[i][j])
-                t.pendown()
-                t.dot()
-                t.penup()
-        draw(branches - 1)
+def draw(distance):
+    if distance < 20:
+        hello = 0 # Return function
     else:
-        return 0
-
-def start():
-    length = 5
-    turtle.screensize(canvwidth=500, canvheight=500)
-    draw(5)
-    #t.goto(-450, 0)
+        turtle.forward(distance) # Forward and turn left
+        turtle.left(35)
+        draw(3 * distance / 4) # recursion 1 for branches
+        turtle.right(70)
+        draw(3 * distance / 4) # recursion 2 for branches
+        turtle.left(35)
+        turtle.backward(distance) # back to original position
 
 while True:
-    draw(10, 5)
-    
+    draw(85)
