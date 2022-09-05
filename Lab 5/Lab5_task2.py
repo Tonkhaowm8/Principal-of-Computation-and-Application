8447a8106a1987ae5912fb5f7cea425b807226b2
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-09-05 23:54:34 +0700

    create both star patterns

--- Lab 5/Lab5_task2.py
+++ Lab 5/Lab5_task2.py
@@ -4,20 +4,13 @@ def stars(n):
         print("*" * stars(n - 1))
     return (n + 1)
 
-#stars(int(input("enter a number: ")))
+stars(int(input("enter a number: ")))
 
-def stars2(n):
-    def stars3(n): # print 1 - 5
-        if n > 0:
-            print("*" * stars(n - 1))
-        return (n + 1)
-    if n > 0:
-        stars2(n - 1)
-        print(n)
-    else:
-        print("*" * stars3(n))
-
-    return (n + 1)
+def stars2(n, a = 1):
+    if n >= a:
+        print('*' * a)
+        stars2(n, a + 1)
+        print('*' * a)
     
 
 stars2(int(input("enter a number: ")))
