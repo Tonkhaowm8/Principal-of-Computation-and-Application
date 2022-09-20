a473519120e41dd262e5c0ce10a90fb7c8d55df7
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-09-20 12:16:34 +0700

    add soi car detection and stuff

--- Lab 7/valletParking.py
+++ Lab 7/valletParking.py
@@ -44,49 +44,93 @@ class parking:
     def park(self, car):
         self.parkingLot.push(car)
 
-    def backOut(self, car):
+    def backOut(self, car, stackB):
+        carList = stackB.returnStack()
         if self.parkingLot.contain(car):
-            carList = stack()
+            origiSize = carList.size()
             for i in range(self.parkingLot.size()):
                 if car == self.parkingLot.peek():
                     self.parkingLot.pop()
+                    print(f"Removing car number {car}...")
+                    print(f"Soi 1: {self.parkingLot.returnLs()}")
+                    print(f"Soi 2: {carList.returnLs()} \n")
                     break
                 else:
-                    carList.push(self.parkingLot.pop())
-            for j in range(carList.size()):
+                    np = self.parkingLot.pop()
+                    carList.push(np)
+                    print(f"moving {np} to Soi 2...")
+                    if carList.size() > 5:
+                        print("Soi Full!!!")
+                        exit()
+                    else:
+                        print(f"Soi 1: {self.parkingLot.returnLs()}")
+                        print(f"Soi 2: {carList.returnLs()} \n")
+            newSize = carList.size()
+            print("Getting the cars back to it's soi... \n")
+            for j in range(newSize - origiSize):
                 self.parkingLot.push(carList.pop())
+                print(f"Soi 1: {self.parkingLot.returnLs()}")
+                print(f"Soi 2: {carList.returnLs()} \n")
+        elif carList.contain(car):
+            origiSize = self.parkingLot.size()
+            for i in range(carList.size()):
+                if car == carList.peek():
+                    carList.pop()
+                    print(f"Removing car number {car}...")
+                    print(f"Soi 1: {self.parkingLot.returnLs()}")
+                    print(f"Soi 2: {carList.returnLs()} \n")
+                    break
+                else:
+                    np = carList.pop()
+                    self.parkingLot.push(np)
+                    if self.parkingLot.size() > 5:
+                        print("Soi Full!!!")
+                        exit()
+                    else:
+                        print(f"moving {np} to Soi 2...")
+                        print(f"Soi 1: {self.parkingLot.returnLs()}")
+                        print(f"Soi 2: {carList.returnLs()} \n")
+            newSize = self.parkingLot.size()
+            print("Getting the cars back to it's soi... \n")
+            for j in range(newSize - origiSize):
+                carList.push(self.parkingLot.pop())
+                print(f"Soi 1: {self.parkingLot.returnLs()}")
+                print(f"Soi 2: {carList.returnLs()} \n")
 
 
 def initialize():
-    soiInput = int(input("How many soi are there to park: "))
+    print("\n")
     soiArr = []
-    for i in range(soiInput):  
+    for i in range(2):  
         soiArr.append(parking())
     for i in soiArr:
-        carNum = random.randint(0, 4)
+        carNum = random.randint(2, 4)
         for j in range(carNum):
             carPlate = random.randint(1, 1000)
             while i.returnStack().contain(carPlate):
                 carPlate = random.randint(1, 1000)
             i.park(carPlate)
         print(i.returnStack().returnLs())
+    print("\n")
     return soiArr
 
 def inputMode(soiArr):
     modeInput = int(input("1. parking, 2. backout: "))
+    print("\n")
     if modeInput == 1:
         carNumber = int(input("Enter car licence plate number: "))
         soiNum = int(input("Enter the number of soi you want to park: ")) - 1
         soiArr[soiNum].park(carNumber)
-        print(f"Your car with the number {carNumber} has been parked!")
+        print(f"Your car with the number {carNumber} has been parked! \n")
         for j in soiArr:
             print(j.returnStack().returnLs())
     elif modeInput == 2:
         carPlate = int(input("Enter the car number you want to backout: "))
-        print(f"Your car with the number {carPlate} has been backed out!")
-        for i in soiArr:
-            i.backOut(carPlate)
-            print(i.returnStack().returnLs())
+        print("\n")
+        for i in range(2):
+            soiArr[0].backOut(carPlate, soiArr[1])
+            print(soiArr[i].returnStack().returnLs())
+        print(f"Your car with the number {carPlate} has been backed out! \n")
     else:
         print("Enter the correct input!!!")
         inputMode
