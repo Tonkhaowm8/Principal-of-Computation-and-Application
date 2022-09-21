ed2524e6af5cd48293c3559aa67c22a3dd49b5d1
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-09-21 10:55:11 +0700

    add comments

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-import turtle
+from array import array
 
-t = turtle.Turtle(shape = "turtle")
 
-while 1:
-    t.goto(200, 200)
+sentence = input("Enter number: ")
+newSentence = sentence.split()
+for j in range(0, len(newSentence) - 1):
+    if "" in newSentence[j] and newSentence[j] == "$":
+        newSentence[j] = "z"
+
+print(newSentence)
+
+
+sentence = input("Enter sentence: ")
+newSentence = sentence.split()
+for i in newSentence:
+    wordList = list(i)
+    if wordList[-1] == "s" or wordList[-1] == "$":
+        wordList[-1] = "z"
+    print("".join(wordList))
