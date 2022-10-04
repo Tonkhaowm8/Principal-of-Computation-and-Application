3c8e05e466d92463a300e2a889d98ab95508382d
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-10-05 00:21:37 +0700

    add function

--- Lab 8/Lab8_postlab.py
+++ Lab 8/Lab8_postlab.py
@@ -30,3 +30,36 @@ class doubleLinkedList:
 
     def add(self, item):
         addItem = Node(item)
+        if self.head == None:
+            addItem.setPrevious(None)
+            addItem.setNext(None)
+            self.head = addItem
+        else:
+            self.head.setNext(addItem)
+            addItem.setPrevious(self.head)
+            addItem.setNext(None)
+            self.head = addItem
+
+    def print(self):
+        current = self.head
+        while current != None:
+            print(current.getData())
+            current = current.getPrevious()
+
+    def sort(self):
+        current = self.head
+        previous = current.getPrevious()
+        while current != None:
+            if current.getData() > previous.getData():
+                current
+
+    
+def start():
+    numPlayer = int(input("Enter the amount of players: "))
+    dbll = doubleLinkedList()
+    for i in range(numPlayer):
+        dataArr = [input("Enter Name: "), int(input("Enter score: "))]
+        dbll.add(dataArr)
+    dbll.print()
+
+start()
