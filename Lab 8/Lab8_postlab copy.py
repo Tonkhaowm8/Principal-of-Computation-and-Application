bf88a6419200f345fa3a7c6aa24ae52199d5e2ed
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-10-12 10:40:25 +0700

    add functions

--- Lab 8/Lab8_postlab copy.py
+++ Lab 8/Lab8_postlab copy.py
@@ -54,12 +54,14 @@ class doubleLinkedList:
         if mode == 1:
             current = self.head
             while current != None:
-                print(current.getData())
+                if current.getData() != None:
+                    print(current.getData())
                 current = current.getPrevious()
         else:
             current = self.tail
             while current != None:
-                print(current.getData())
+                if current.getData() != None:
+                    print(current.getData())
                 current = current.getNext()
         
 
@@ -78,11 +80,22 @@ class doubleLinkedList:
                     previousNode = previousNode.getPrevious()
                 current = current.getPrevious()
 
-    def addScore(self, name, score):
+    def retrieveScore(self, name):
         current = self.head
         found = True
         while current.getData()[0] != name:
-            current = current.getNext()
+            current = current.getPrevious()
+            if current == None:
+                found = False
+                break
+        if found:
+            return current.getData()
+
+    def editScore(self, name, score):
+        current = self.head
+        found = True
+        while current.getData()[0] != name:
+            current = current.getPrevious()
             if current == None:
                 found = False
                 break
@@ -91,14 +104,28 @@ class doubleLinkedList:
             oldData[1] = score
             current.setData(oldData)
 
+    def deleteScore(self, name):
+        current = self.head
+        found = True
+        while current.getData()[0] != name:
+            current = current.getPrevious()
+            if current == None:
+                found = False
+                break
+        if found:
+            while current.getNext() != None:
+                current.setData(current.getNext().getData())
+                current = current.getNext()
+            current.setData(None)
+
 def start():
     numPlayer = int(input("Enter the amount of players: "))
     dbll = doubleLinkedList()
     for i in range(numPlayer):
         dataArr = [input("Enter Name: "), int(input("Enter score: "))]
         dbll.add(dataArr)
-    dbll.print()
-    mode = int(input("[1] Sort, [2] Add, [3] Delete: "))
+    dbll.print(1)
+    mode = int(input("[1] Sort, [2] Edit, [3] Add, [4] Delete, [5] Retrieve: "))
     if mode == 1:
         dbll.sort()
         userMode = int(input("[1] ascending, [2] decending: "))
@@ -106,8 +133,19 @@ def start():
     elif mode == 2:
         name = input("Enter name of player: ")
         score = int(input("Enter score: "))
-        dbll.addScore(name, score)
+        dbll.editScore(name, score)
         dbll.print(1)
-
+    elif mode == 3:
+        name = input("Enter name of player: ")
+        score = int(input("Enter score: "))
+        dbll.add([name, score])
+        dbll.print(1)
+    elif mode == 4:
+        name = input("Enter name of player: ")
+        dbll.deleteScore(name)
+        dbll.print(1)
+    elif mode == 5:
+        name = input("Enter name of player: ")
+        print(dbll.retrieveScore(name))
 
 start()
