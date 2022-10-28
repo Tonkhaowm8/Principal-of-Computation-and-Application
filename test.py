aeb740c8e03c13335daa152d1ab01ef094087330
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-10-28 23:16:13 +0700

    edit test

--- test.py
+++ test.py
@@ -1,19 +1,2 @@
-from array import array
-
-
-sentence = input("Enter number: ")
-newSentence = sentence.split()
-for j in range(0, len(newSentence) - 1):
-    if "" in newSentence[j] and newSentence[j] == "$":
-        newSentence[j] = "z"
-
-print(newSentence)
-
-
-sentence = input("Enter sentence: ")
-newSentence = sentence.split()
-for i in newSentence:
-    wordList = list(i)
-    if wordList[-1] == "s" or wordList[-1] == "$":
-        wordList[-1] = "z"
-    print("".join(wordList))
+a = "hello"
+print(a[2:-2])
