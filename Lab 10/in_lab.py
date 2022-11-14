b56bd5632ca5a27969baf97d1e809b67e538ed0c
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-11-15 00:52:00 +0700

    add stuff

--- Lab 10/in_lab.py
+++ Lab 10/in_lab.py
@@ -1,10 +1,32 @@
 import os
+import math
 
-file = open(r"C:\Users\Tonkhaow\Desktop\PCA\Principal-of-Computation-and-Application\Lab 10\small.txt", "r")
-data = file.read()
-file.close()
-data = data.split(" ")
-print(data)
+class node:
+    def __init__():
+        next = None
+        data = None
+
+class hashtable:
+    def __init__(arr):
+        hash_table = []
+        length = len(arr) * 0.2
+
+def getNextPrime(num):
+    prime = False
+    while not prime:
+        if '.' in str(math.sqrt(num)):
+            for i in range(1, 9):
+                if num % i != 0:
+                    num += 1
+                    prime = False
+                    break
+                else:
+                    prime = True
+                    continue
+            
+        else:
+            num += 1
+    return num
 
 def hash(str):
     h = 0
@@ -12,3 +34,10 @@ def hash(str):
         h *= 37
         h += ord(ch)
     return h
+
+file = open(r"C:\Users\Tonkhaow\Desktop\PCA\Principal-of-Computation-and-Application\Lab 10\small.txt", "r")
+data = file.read()
+file.close()
+data = data.split(" ")
+
+print(getNextPrime(25))
